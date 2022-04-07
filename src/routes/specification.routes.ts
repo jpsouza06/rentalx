2fa5bd74fa175890b86a6a0ee1e032208483f665
Joao Paulo <joaopaulosc06@gmail.com>
2022-04-07 20:32:08 -0300

    Update

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,27 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecifications";
 
 const specificationRoutes = Router();
 
-const specificationRepository = new SpecificationRepository();
-
 specificationRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
-});
-
-specificationRoutes.get("/", (request, response) => {
-  const all = specificationRepository.list();
-
-  return response.json(all);
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationRoutes };
